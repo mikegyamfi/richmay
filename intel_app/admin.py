6427ec4eb578616692652866350baf862960d8b1
mikegyamfi <codeage20@gmail.com>
2025-02-27 18:22:53 +0000

    topup list change

--- intel_app/admin.py
+++ intel_app/admin.py
@@ -1,7 +1,20 @@
-from django.contrib import admin
+from django import forms
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
+from django.db import transaction
+
 from . import models
 from import_export.admin import ExportActionMixin
+
+from .models import CustomUser, WalletTransaction
+
+
+class CustomUserAdminForm(forms.ModelForm):
+    add_balance = forms.FloatField(required=False, label="Add Balance", help_text="Add to user's wallet balance.")
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
 
 
 # Register your models here.
@@ -25,6 +38,39 @@ class CustomUserAdmin(ExportActionMixin, UserAdmin):
             'classes': ('wide',),
             'fields': ('username', 'password1', 'password2', 'wallet')
         }),)
+
+    def save_model(self, request, obj, form, change):
+        add_balance = form.cleaned_data.get('add_balance', 0)
+        if add_balance and add_balance > 0:
+            try:
+                with transaction.atomic():
+                    # Lock the user record for updating the balance
+                    obj = CustomUser.objects.select_for_update().get(id=obj.id)
+
+                    # Update the user's wallet balance
+                    old_balance = obj.wallet
+                    new_balance = old_balance + add_balance
+                    obj.wallet = new_balance
+                    obj.save()
+
+                    # Create a WalletTransaction record
+                    WalletTransaction.objects.create(
+                        user=obj,
+                        transaction_type="Credit",
+                        transaction_amount=add_balance,
+                        transaction_use="Admin Wallet Addition",
+                        new_balance=new_balance,
+                    )
+
+                    # Send a success message
+                    messages.success(request, f"Successfully added GHS {add_balance} to {obj.username}'s wallet.")
+                    message = f"GHS{add_balance} has been added to your account"
+                    # send_sms_task.delay(obj.phone, message)
+            except Exception as e:
+                messages.error(request, f"Failed to add balance: {str(e)}")
+
+        super().save_model(request, obj, form, change)
+
 
 
 class IShareBundleTransactionAdmin(admin.ModelAdmin):
